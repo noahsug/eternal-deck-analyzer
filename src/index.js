eab729b3a0ae8304b61d92a3591e73f072303441
noah_sugarman <noah.sugarman@airbnb.com>
2018-04-08 22:02:16 -0700

    add stats

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import './style.css'
 const inputNode = document.querySelector('textarea.input')
 const overallNode = document.querySelector('.overall')
 const breakdownNode = document.querySelector('.breakdown')
+const greaterThan3Node = document.querySelector('.gt3')
+const greaterThan2Node = document.querySelector('.gt2')
+const greaterThan2AvgNode = document.querySelector('.gt2-avg')
 
 inputNode.addEventListener('paste', onInputChange)
 inputNode.addEventListener('change', onInputChange)
@@ -25,6 +28,7 @@ function parseUri() {
   const cards = parseDeck(input)
   updateRating(cards)
   updateBreakdown(cards)
+  updateGreaterThan(cards)
 }
 
 function updateRating(cards) {
@@ -49,13 +53,29 @@ function updateBreakdown(cards) {
     .sort()
     .reverse()
   sortedRatings.forEach(rating => {
-    html.push(`<div><h3>${rating}</h3><ul>`)
+    html.push(`<div><div><h3>${rating}</h3>`)
+    html.push(`<span class="count">(${ratingGroups[rating].length})</span>`)
+    html.push(`</div><ul>`)
     ratingGroups[rating].forEach(card => {
       html.push(`<li>${card}</li>`)
     })
     html.push(`</ul></div>`)
   })
   breakdownNode.innerHTML = html.join('')
+}
+
+function updateGreaterThan(cards) {
+  const greaterThan3 = cards.filter(card => getRating(card) >= 3)
+  greaterThan3Node.innerHTML = greaterThan3.length
+
+  const greaterThan2 = cards.filter(card => getRating(card) >= 2)
+  greaterThan2Node.innerHTML = greaterThan2.length
+
+  const total = greaterThan2.reduce((total, prev) => {
+    return total + getRating(prev)
+  }, 0)
+
+  greaterThan2AvgNode.innerHTML = (total / greaterThan2.length).toFixed(2)
 }
 
 parseUri()
